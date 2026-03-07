ce56da9a8166682fe338f6b5ad45a8c321361426
aleksana <me@aleksana.moe>
2026-03-07 21:01:05 +0800

    mainline-chromeos-sc7180: bump to 6.12.76 and add hj110iz fix

--- devices/families/mainline-chromeos-sc7180/kernel/default.nix
+++ devices/families/mainline-chromeos-sc7180/kernel/default.nix
@@ -1,16 +1,15 @@
 { mobile-nixos
 , fetchzip
-, fetchpatch
 , ...
 }:
 
 mobile-nixos.kernel-builder rec {
-  version = "6.12.19";
+  version = "6.12.76";
   configfile = ./config.aarch64;
 
   src = fetchzip {
     url = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-${version}.tar.xz";
-    hash = "sha256-9Uq2kgoSe42EPUEwlEP2ai8c8VFl+aZ/DTPrwBHagyY=";
+    hash = "sha256-GnC2A8ja45gifcnkmT4LcorgFWr6pBLoWlT95eD1rEA=";
   };
 
   patches = [
@@ -19,6 +18,8 @@ mobile-nixos.kernel-builder rec {
     ./readd_mrbland_rev1.patch
 
     ./remove_bwmon_nodes.patch
+
+    ./fix_compat_old_hj110iz.patch
   ];
 
   isModular = true;
